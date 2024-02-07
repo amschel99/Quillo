e5472c85808ac60f854b6f1031d1600fab44ee06
mburuanthony <mburuantony@outlook.com>
2024-02-07 11:53:24 +0300

    remove frontend

--- src/quillo_frontend/src/screens/Onboarding.tsx
+++
@@ -1,7 +0,0 @@
-import React,{ JSX } from "react";
-import { CompanyProfile } from "../components/auth/CompanyProfile";
-
-const onBoardingScreen = (): JSX.Element => {
-  return <CompanyProfile />;
-};
-export default onBoardingScreen;
