da12f2d22efdeee990ae2578f25f504b2ee0e702
amschel99 <6290.2020@students.ku.ac.ke>
2024-02-08 11:54:10 +0300

    signup company and assign dao to the company

--- src/quillo_backend/src/dao/types.rs
+++ src/quillo_backend/src/dao/types.rs
@@ -3,15 +3,16 @@ use std::borrow::Cow;
 use crate::global_types::*;
 use candid::{CandidType, Decode, Encode, Principal};
 use ic_stable_structures::{BoundedStorable, Storable};
-use icrc_ledger_types::icrc1::account::Account;
+use icrc_ledger_types::icrc1::{account::Account, transfer::NumTokens};
 
 #[derive(candid::CandidType, Clone, Serialize, Deserialize)]
 
 pub struct Dao {
-    pub accounts: Vec<Account>,
-
+    pub accounts: Option<Vec<Account>>,
     pub total_shares: NumTokens,
-    pub id: Principal,
+    pub id: u64,
+    pub system_params: SystemParams,
+    pub proposals: Option<Vec<Proposal>>,
 }
 
 impl Storable for Dao {
@@ -27,4 +28,80 @@ impl Storable for Dao {
 impl BoundedStorable for Dao {
     const MAX_SIZE: u32 = 1024;
     const IS_FIXED_SIZE: bool = false;
+}
+
+#[derive(Clone, Debug, CandidType, Deserialize, Serialize)]
+pub struct Proposal {
+    pub id: u64,
+    pub timestamp: u64,
+    pub proposer: Principal,
+    pub payload: ProposalPayload,
+    pub state: ProposalState,
+    pub votes_yes: NumTokens,
+    pub votes_no: NumTokens,
+    pub voters: Vec<Principal>,
+}
+#[derive(Clone, Debug, CandidType, Deserialize, Serialize)]
+pub struct ProposalPayload {
+    pub canister_id: Principal,
+    pub method: String,
+    pub message: Vec<u8>,
+}
+// The state of a Proposal
+#[derive(Clone, Debug, CandidType, Deserialize, PartialEq, Serialize)]
+pub enum ProposalState {
+    // The proposal is open for voting
+    Open,
+
+    // Enough "yes" votes have been cast to accept the proposal, and it will soon be executed
+    Accepted,
+
+    // Enough "no" votes have been cast to reject the proposal, and it will not be executed
+    Rejected,
+
+    // The proposal is currently being executed
+    Executing,
+
+    // The proposal has been successfully executed
+    Succeeded,
+
+    // A failure occurred while executing the proposal
+    Failed(String),
+}
+#[derive(Clone, Debug, CandidType, Deserialize, Serialize)]
+pub enum Vote {
+    Yes,
+    No,
+}
+
+#[derive(Clone, Debug, CandidType, Deserialize, Serialize)]
+pub struct TransferArgs {
+    pub to: Principal,
+    pub amount: NumTokens,
+}
+
+#[derive(Clone, Debug, CandidType, Deserialize, Serialize)]
+pub struct VoteArgs {
+    pub proposal_id: u64,
+    pub vote: Vote,
+}
+
+#[derive(Clone, Default, Debug, CandidType, Deserialize, Serialize)]
+pub struct SystemParams {
+    // The fee incurred by transferring tokens
+    pub transfer_fee: NumTokens,
+
+    // The amount of tokens needed to vote "yes" to accept, or "no" to reject, a proposal
+    pub proposal_vote_threshold: NumTokens,
+
+    // The amount of tokens that will be temporarily deducted from the account of
+    // a user that submits a proposal. If the proposal is Accepted, this deposit is returned,
+    // otherwise it is lost. This prevents users from submitting superfluous proposals.
+    pub proposal_submission_deposit: NumTokens,
+}
+#[derive(Clone, Debug, CandidType, Deserialize, Serialize)]
+pub struct UpdateSystemParamsPayload {
+    pub transfer_fee: Option<NumTokens>,
+    pub proposal_vote_threshold: Option<NumTokens>,
+    pub proposal_submission_deposit: Option<NumTokens>,
 }
--- src/quillo_backend/src/lib.rs
+++ src/quillo_backend/src/lib.rs
@@ -2,6 +2,7 @@
 #[macro_use]
 extern crate serde;
 use candid::{Decode, Encode, Principal};
+use dao::service::InitPayload;
 //use company::CompanyInformation;
 use ic_cdk::api::time;
 use ic_stable_structures::memory_manager::{MemoryId, MemoryManager, VirtualMemory};
@@ -16,7 +17,7 @@ mod dao;
 mod global_types;
 
 use company::*;
-use dao::types::Dao;
+use dao::types::{Dao, SystemParams};
 use global_types::*;
 
 type Memory = VirtualMemory<DefaultMemoryImpl>;
@@ -47,6 +48,7 @@ static COMPANYSTORAGE: RefCell<StableBTreeMap<u64, CompanyInformation, Memory>> 
 
 
  }
+
 #[ic_cdk::update]
 
 fn signup_company(payload: CompanyInformation) {
@@ -55,7 +57,7 @@ fn signup_company(payload: CompanyInformation) {
         let _ = counter.borrow_mut().set(counter_value + 1);
         counter_value
     });
-    let company = CompanyInformation {
+    let mut company = CompanyInformation {
         company_name: payload.company_name,
         company_description: payload.company_description,
         industry: payload.industry,
@@ -66,10 +68,42 @@ fn signup_company(payload: CompanyInformation) {
         id,
         tokenization_info: payload.tokenization_info,
         principal: ic_cdk::caller(),
+        dao_id: None,
     };
+    let governance_params = SystemParams {
+        proposal_submission_deposit: 10.into(),
+        proposal_vote_threshold: 10.into(),
+        transfer_fee: 10.into(),
+    };
+    company.dao_id = Some(_assign_dao(&company, governance_params));
 }
 //Helper function to insert a company
 fn _signup_company(company: &CompanyInformation) {
     COMPANYSTORAGE.with(|service| service.borrow_mut().insert(company.id, company.clone()));
-    // Now `companies` should have the correct lifetime
+}
+
+fn _assign_dao(company: &CompanyInformation, governance_params: SystemParams) -> u64 {
+    //TODO Create a dao for the given company
+
+    let id = ID_COUNTER.with(|counter| {
+        let counter_value = *counter.borrow().get();
+        let _ = counter.borrow_mut().set(counter_value + 1);
+        counter_value
+    });
+
+    let dao_payload = InitPayload {
+        id,
+        system_params: governance_params,
+        total_shares: company.tokenization_info.class.value().into(),
+    };
+    let created_dao = Dao::new(dao_payload);
+    //SAVE THE DAO IN THE PERMANENT STORAGE
+    save_dao(created_dao);
+
+    id
+}
+
+#[ic_cdk::update]
+fn save_dao(dao: Dao) {
+    DAOSTORAGE.with(|service| service.borrow_mut().insert(dao.id, dao.clone()));
 }
