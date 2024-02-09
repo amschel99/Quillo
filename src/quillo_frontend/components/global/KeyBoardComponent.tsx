b7ed9ed1f99dfde19ff3f0e943a46bbdd12c99c6
devitoDbug <davidOchiengy@gmail.com>
2024-02-09 22:59:14 +0300

    feat: Refactor KeyBoardComponent to accept props  -Added a feature to enable a decimal point to only be clicked once in a given number.  -Enabled erasing of digits from right to right

--- src/quillo_frontend/components/global/KeyBoardComponent.tsx
+++ src/quillo_frontend/components/global/KeyBoardComponent.tsx
@@ -3,48 +3,124 @@ import React from "react";
 import { colors, textbold } from "../../assets/constants";
 import { BackSpaceIcon } from "../../assets/icons";
 
-export const KeyBoardComponent = (): JSX.Element => {
+export interface KeyBoardComponentProps {
+  handleIncludeValue: (digit: string) => void;
+  handlePopeValue: () => void;
+  setDecimalClicked: React.Dispatch<React.SetStateAction<boolean>>;
+  decimalClicked: boolean;
+  setDecimalPlaceIndex: React.Dispatch<React.SetStateAction<number>>;
+  numberOfQuilloShares: string;
+}
+
+export const KeyBoardComponent = ({
+  handleIncludeValue,
+  handlePopeValue,
+  setDecimalClicked,
+  setDecimalPlaceIndex,
+  decimalClicked,
+  numberOfQuilloShares,
+}: KeyBoardComponentProps): JSX.Element => {
   return (
     <View style={styles.container}>
       <View style={styles.row}>
-        <TouchableOpacity style={styles.item}>
+        <TouchableOpacity
+          style={styles.item}
+          onPress={() => {
+            handleIncludeValue("1");
+          }}
+        >
           <Text style={textbold}>1</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.item}>
+        <TouchableOpacity
+          style={styles.item}
+          onPress={() => {
+            handleIncludeValue("2");
+          }}
+        >
           <Text style={textbold}>2</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.item}>
+        <TouchableOpacity
+          style={styles.item}
+          onPress={() => {
+            handleIncludeValue("3");
+          }}
+        >
           <Text style={textbold}>3</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.item}>
+        <TouchableOpacity
+          style={styles.item}
+          onPress={() => {
+            handleIncludeValue("4");
+          }}
+        >
           <Text style={textbold}>4</Text>
         </TouchableOpacity>
       </View>
       <View style={styles.row}>
-        <TouchableOpacity style={styles.item}>
+        <TouchableOpacity
+          style={styles.item}
+          onPress={() => {
+            handleIncludeValue("5");
+          }}
+        >
           <Text style={textbold}>5</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.item}>
+        <TouchableOpacity
+          style={styles.item}
+          onPress={() => {
+            handleIncludeValue("6");
+          }}
+        >
           <Text style={textbold}>6</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.item}>
+        <TouchableOpacity
+          style={styles.item}
+          onPress={() => {
+            handleIncludeValue("7");
+          }}
+        >
           <Text style={textbold}>7</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.item}>
+        <TouchableOpacity
+          style={styles.item}
+          onPress={() => {
+            handleIncludeValue("8");
+          }}
+        >
           <Text style={textbold}>8</Text>
         </TouchableOpacity>
       </View>
       <View style={styles.row}>
-        <TouchableOpacity style={styles.item}>
+        <TouchableOpacity
+          style={styles.item}
+          onPress={() => {
+            handleIncludeValue("9");
+          }}
+        >
           <Text style={textbold}>9</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.item}>
+        <TouchableOpacity
+          style={styles.item}
+          onPress={() => {
+            handleIncludeValue("0");
+          }}
+        >
           <Text style={textbold}>0</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.item}>
+        <TouchableOpacity
+          style={styles.item}
+          onPress={() => {
+            if (decimalClicked) {
+              return;
+            }
+            setDecimalClicked(true);
+            setDecimalPlaceIndex(numberOfQuilloShares.length);
+            handleIncludeValue(".");
+          }}
+        >
           <Text style={textbold}>.</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.item}>
+        <TouchableOpacity style={styles.item} onPress={handlePopeValue}>
           <BackSpaceIcon />
         </TouchableOpacity>
       </View>
--- src/quillo_frontend/screens/investor/BuySharesScreen.tsx
+++ src/quillo_frontend/screens/investor/BuySharesScreen.tsx
@@ -1,12 +1,25 @@
-import { JSX } from "react";
-import { StyleSheet, ScrollView, View, TextInput, Text } from "react-native";
+import { JSX, useState } from "react";
+import {
+  StyleSheet,
+  ScrollView,
+  View,
+  TouchableOpacity,
+  Text,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import type { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../../navigation";
 import { AppHeader } from "../../components/global/AppHeader";
 import { Button } from "../../components/global/Button";
 import { KeyBoardComponent } from "../../components/global/KeyBoardComponent";
-import { screenstyle, textbold } from "../../assets/constants";
+import { SCREENWIDTH, colors } from "../../assets/constants";
+import {
+  screenstyle,
+  textbold,
+  textlight,
+  textlightfaint,
+  textlightfaintbold,
+} from "../../assets/constants";
 import { ArrowRightIcon } from "../../assets/icons";
 
 type buysharesProps = NativeStackScreenProps<RootStackParamList, "buyshares">;
@@ -15,24 +28,111 @@ export default function BuySharesScreen({
   route,
   navigation,
 }: buysharesProps): JSX.Element {
+  const [numberOfQuilloShares, setNumberOfQuilloShares] = useState("");
+  const [decimalClicked, setDecimalClicked] = useState(false);
+  const [decimalPlaceIndex, setDecimalPlaceIndex] = useState<number>(-1);
   const { companyId } = route.params;
 
   const goBack = (): void => navigation.goBack();
 
   const handlePress = () => {};
 
+  const handleIncludeValue = (digit: string) => {
+    setNumberOfQuilloShares((prev) => prev + digit);
+  };
+
+  const handlePopeValue = () => {
+    if (numberOfQuilloShares.length <= decimalPlaceIndex + 1) {
+      // Resetting the decimal place clicked state because it has been erased
+      // Recording the index of the decimal place in the string
+      setDecimalClicked(false);
+      setDecimalPlaceIndex(-1);
+    }
+    setNumberOfQuilloShares((prev) =>
+      prev.substring(0, numberOfQuilloShares.length - 1)
+    );
+  };
+
   return (
     <SafeAreaView style={styles.screenContainer}>
       <AppHeader renderSearch={true} />
       <ScrollView>
-        <Text style={textbold}>Get Quillo Shares</Text>
-        <KeyBoardComponent />
+        <Text style={{ ...textbold, marginBottom: 16, paddingHorizontal: 8 }}>
+          Get Quillo Shares
+        </Text>
+        <Text
+          style={{
+            ...textlightfaintbold,
+            fontSize: 16,
+            marginBottom: 16,
+            paddingHorizontal: 8,
+          }}
+        >
+          Share distribution
+        </Text>
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            gap: 32,
+            marginBottom: 16,
+            paddingHorizontal: 8,
+          }}
+        >
+          <View>
+            <Text style={{ ...textlightfaint, fontSize: 16 }}>
+              Available shares
+            </Text>
+            <Text style={textlight}>6000000</Text>
+          </View>
+          <View>
+            <Text style={{ ...textlightfaint, fontSize: 16 }}>
+              Share distribution
+            </Text>
+            <Text style={textlight}>2 Tokens/Share</Text>
+          </View>
+        </View>
+        <View style={styles.divider} />
+        <Text
+          style={{
+            ...textlightfaintbold,
+            fontSize: 16,
+            marginBottom: 16,
+            paddingHorizontal: 8,
+          }}
+        >
+          How many Quillo shares would you like to acquire ?
+        </Text>
+        <TouchableOpacity style={styles.inputBox}>
+          <Text style={{ ...textlightfaint, fontSize: 16 }}>
+            {numberOfQuilloShares}
+          </Text>
+        </TouchableOpacity>
+        <Text
+          style={{
+            ...textlight,
+            fontSize: 16,
+            paddingHorizontal: 8,
+            marginBottom: 48,
+          }}
+        >
+          200 shares = 400 tokens
+        </Text>
+        <KeyBoardComponent
+          handleIncludeValue={handleIncludeValue}
+          handlePopeValue={handlePopeValue}
+          decimalClicked={decimalClicked}
+          setDecimalClicked={setDecimalClicked}
+          numberOfQuilloShares={numberOfQuilloShares}
+          setDecimalPlaceIndex={setDecimalPlaceIndex}
+        />
       </ScrollView>
       <Button
         dpText={"Get Quillo Shares"}
         dpIcon={<ArrowRightIcon />}
         isDisabled={false}
         onPressFunc={handlePress}
+        xstyles={{ borderRadius: 0, marginBottom: 8 }}
       />
     </SafeAreaView>
   );
@@ -43,5 +143,22 @@ const styles = StyleSheet.create({
     ...screenstyle,
     paddingHorizontal: 0,
     paddingBottom: 0,
+  },
+  divider: {
+    width: SCREENWIDTH,
+    borderBottomWidth: 1,
+    marginBottom: 16,
+    borderColor: colors.divider_faint,
+  },
+  inputBox: {
+    height: 44,
+    borderColor: colors.divider_faint,
+    backgroundColor: colors.divider,
+    paddingHorizontal: 8,
+    paddingVertical: 11,
+    marginHorizontal: 8,
+    marginBottom: 8,
+    borderWidth: 1,
+    borderRadius: 6,
   },
 });
